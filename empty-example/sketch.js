7c182b8dfaab3f0b7db65cedf984cb6ac75f4e85
Jan Chráska <jan.chraska@gmail.com>
2020-09-15 16:49:17 +0200

    SMarouProject

---
+++ empty-example/sketch.js
@@ -0,0 +1,19 @@
+let x;
+let y;
+
+function setup() {
+  // put setup code here
+  createCanvas(800, 800);
+  clear();
+  background(128);
+  x = width / 2;
+  y = height / 2;
+
+}
+
+function draw() {
+  // put drawing code here
+  clear();
+  background(128);
+  circle(x, y, 50);
+}
